053615b2b6a3d6773ecea4667ff6b35a145c1b0c
Dean Young <dlyoung119@me.com>
2019-10-05 20:30:42 -0700

    function

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ Awair.prototype = {
 								.setCharacteristic(Characteristic.CarbonDioxideLevel, parseFloat(sensors[sensor]))
 							
 							var co2Detected;
-							var co2Before = that.carbonDioxideService.getCharacteristic(Characteristic.CarbonDioxideDetected).on('get', that.getCarbonDioxideState.bind(that)).value();
+							var co2Before = that.carbonDioxideService.getCharacteristic(Characteristic.CarbonDioxideDetected).on('get', that.getCarbonDioxideState.bind(that));
+							co2Before = co2Before.value();
 							
 							if (co2Before) {
 								// do nothing
